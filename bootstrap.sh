395ec4f010de7001945da6aaad3548122d37ba4e
Carlos Baston <Carlos.Baston@people10.com>
2019-03-25 10:33:39 -0700

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,43 +6,20 @@ apt-get upgrade
 # Basic Linux Stuff
 apt-get install -y git
 
+# make backup copy of index.html
+cd /var/www/html
+mv index.html index-backup.html
+
 # Apache
 apt-get install -y apache2
 
 # Enable Apache Mods
 a2enmod rewrite
 
-#Add Onrej PPA Repo
-apt-add-repository ppa:ondrej/php
-apt-get update
-
-# Install PHP
-apt-get install -y php7.2
-
-# PHP Apache Mod
-apt-get install -y libapache2-mod-php7.2
-
-# Restart Apache
-service apache2 restart
-
-# PHP Mods
-apt-get install -y php7.2-common
-apt-get install -y php7.2-mcrypt
-apt-get install -y php7.2-zip
-
-# Set MySQL Pass
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-
-# Install MySQL
-apt-get install -y mysql-server
-
-# PHP-MYSQL lib
-apt-get install -y php7.2-mysql
-
-# remove unewanted index.html
-# cd /var/www/html
-# rm index.html
+# remove unewanted apache index.html and restore original
+cd /var/www/html
+rm index.html
+mv index-backup.html index.html
 
 # Restart Apache
 sudo service apache2 restart
